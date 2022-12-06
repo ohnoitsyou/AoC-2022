1787e73dbe1c9e0c4c051967b00dfd138ff431dc
David Young <ohnoitsyou7043@gmail.com>
2022-12-06 00:46:20 -0500

    Slight optimization to the marker logic
    Looking over the megathread I realized the findMarker function could be
    a one-liner by using indexOfFirst

--- src/main/kotlin/dev/dayoung/Six.kt
+++ src/main/kotlin/dev/dayoung/Six.kt
@@ -2,23 +2,17 @@ package dev.dayoung
 
 class Six(sampleMode: Boolean) {
     private val content = Utils.readInputResource(sampleMode, "six.txt")
-    private fun findUniqueSequence(line: String, windowSize: Int): Int {
-        val windows = line.windowed(windowSize)
-        for ((idx, window) in windows.withIndex()) {
-            if (window.toSet().size == windowSize) {
-                return idx + windowSize
-            }
-        }
-        return 0
-    }
+    private fun findMarker(line: String, windowSize: Int): Int =
+        line.windowed(windowSize).indexOfFirst { it.toSet().size == windowSize } + windowSize
+
     private fun part1() {
         for(entry in content) {
-            println("Part 1: ${findUniqueSequence(entry,4)}")
+            println("Part 1: ${findMarker(entry,4)}")
         }
     }
     private fun part2() {
         for(entry in content) {
-            println("Part 2: ${findUniqueSequence(entry, 14)}")
+            println("Part 2: ${findMarker(entry, 14)}")
         }
     }
     fun solve() {
