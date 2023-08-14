a38d77df95e223914b8ff7e0d27ab4f0181a603e
David Young <ohnoitsyou7043@gmail.com>
2023-08-14 18:09:52 -0400

    Save Day 6

--- src/main/kotlin/dev/dayoung/Six.kt
+++ src/main/kotlin/dev/dayoung/Six.kt
@@ -2,19 +2,9 @@ package dev.dayoung
 
 class Six(sampleMode: Boolean) {
     private val content = Utils.readInputResource(sampleMode, "six.txt")
-    private fun findMarker(line: String, windowSize: Int): Int =
-        line.windowed(windowSize).indexOfFirst { it.toSet().size == windowSize } + windowSize
-
-    private fun part1() {
-        for(entry in content) {
-            println("Part 1: ${findMarker(entry,4)}")
-        }
-    }
-    private fun part2() {
-        for(entry in content) {
-            println("Part 2: ${findMarker(entry, 14)}")
-        }
-    }
+    private fun findMarker(line: String, window: Int): Int = line.windowed(window).indexOfFirst { it.toSet().size == window } + window
+    private fun part1() = content.forEach { println("Part 1: ${findMarker(it,4)}")}
+    private fun part2() = content.forEach { println("Part 2: ${findMarker(it, 14)}") }
     fun solve() {
         part1()
         part2()
