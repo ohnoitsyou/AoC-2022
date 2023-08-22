469c7f777c5d221753ac9defbcf40ac8ba2280b4
David Young <dayoun9@uwe.nsa.gov>
2023-08-22 21:56:04 +0000

    Progress on day 11

--- src/main/kotlin/dev/dayoung/Eleven.kt
+++ src/main/kotlin/dev/dayoung/Eleven.kt
@@ -4,7 +4,7 @@ import kotlin.math.floor
 
 fun List<String>.toMonkey(): Eleven.Monkey {
     val number = this[0].substring("Monkey ".length, this[0].length - 1).toInt()
-    val items = this[1].substring(this[1].lastIndexOf(':') + 1).split(",").map { it.trim().toInt() }
+    val items = this[1].substring(this[1].lastIndexOf(':') + 1).split(",").map { it.trim().toDouble() }
     val operation = this[2].substring(this[2].lastIndexOf('=') + 2).split(" ").map { it.trim()}
     val test = this[3].substring(this[3].lastIndexOf(" ")).trim().toInt()
     val t = this[4].substring(this[4].lastIndexOf(" ")).trim().toInt()
@@ -13,47 +13,49 @@ fun List<String>.toMonkey(): Eleven.Monkey {
 }
 
 fun String.isNumeric() = this.toIntOrNull() != null
-fun String.operatorToIntOp(): Int.(Int) -> Int = when (this) {
-    "+" -> Int::plus
-    "-" -> Int::minus
-    "*" -> Int::times
-    "/","divisible" -> Int::div
+fun String.operatorToIntOp(): Double.(Double) -> Double = when (this) {
+    "+" -> Double::plus
+    "-" -> Double::minus
+    "*" -> Double::times
+    "/","divisible" -> Double::div
     else -> error("Unknown operator $this")
 }
 class Eleven(sampleMode: Boolean) {
     class Operation(val factor1: String, val operator: String, val factor2: String) {
     }
     class Test(val divisor: Int, val t: Int, val f: Int) { }
-    class Monkey(val number: Int, val items: MutableList<Int>, val operation: Operation, val test: Test) {
+    class Monkey(val number: Int, val items: MutableList<Double>, val operation: Operation, val test: Test) {
         var inspectCount = 0
-        fun receiveItem(item: Int) {
+        fun receiveItem(item: Double) {
             items.add(item)
         }
 
-        fun inspectItems(): List<Pair<Int, Int>> {
-            val returnItems = mutableListOf<Pair<Int, Int>>()
+        fun inspectItems(relief: Boolean = true): List<Pair<Int, Double>> {
+            val returnItems = mutableListOf<Pair<Int, Double>>()
             for(item in items) {
-                println("Inspecting item with a worry level of $item")
+//                println("Inspecting item with a worry level of $item")
                 inspectCount++
-                val new = floor(
-                    (if (operation.factor2.isNumeric()) {
-                        operation.operator.operatorToIntOp()(item, operation.factor2.toInt())
-                    } else {
-                        operation.operator.operatorToIntOp()(item, item)
-                    } / 3).toDouble()
-                ).toInt()
-                println("New worry level: $new")
-
-                if((new % test.divisor) == 0) {
-                    println("divisible by ${test.divisor}")
-                    returnItems.add(test.t to new)
-                    println("item with worry level $new is thrown to ${test.t}")
+                var newWorry = if (operation.factor2.isNumeric()) {
+                    operation.operator.operatorToIntOp()(item, operation.factor2.toDouble())
                 } else {
-                    println("not divisible by ${test.divisor}")
-                    returnItems.add(test.f to new)
-                    println("item with worry level $new is thrown to ${test.f}")
+                    operation.operator.operatorToIntOp()(item, item)
                 }
-                println("========")
+                if(relief) {
+                    floor(newWorry)
+                    newWorry = floor(newWorry / 3.0)
+                }
+//                println("New worry level: $newWorry")
+
+                if((newWorry % test.divisor) == 0.0) {
+//                    println("divisible by ${test.divisor}")
+                    returnItems.add(test.t to newWorry)
+//                    println("item with worry level $newWorry is thrown to ${test.t}")
+                } else {
+//                    println("not divisible by ${test.divisor}")
+                    returnItems.add(test.f to newWorry)
+//                    println("item with worry level $newWorry is thrown to ${test.f}")
+                }
+//                println("========")
             }
             items.clear()
             return returnItems
@@ -61,20 +63,29 @@ class Eleven(sampleMode: Boolean) {
     }
 
     private val content = Utils.readInputResource(sampleMode, "eleven.txt")
-    fun part1(monkeys: List<Monkey>) {
-        for(i in 0 until 20) {
+    private fun doSolve(monkeys: List<Monkey>, rounds: Int, relief: Boolean = true) {
+        for(i in 0 until rounds) {
             for(monkey in monkeys) {
-                val thrownItems = monkey.inspectItems()
+                val thrownItems = monkey.inspectItems(relief)
                 thrownItems.forEach { monkeys[it.first].receiveItem(it.second) }
             }
+            monkeys.forEach {
+                println("Monkey ${it.number}: ${it.items}")
+            }
+            if(i + 1 in listOf(1,20,1000,2000,3000,4000,5000,6000,7000,8000,9000,10000)) {
+                println("== After round ${i +1} ==")
+                monkeys.forEachIndexed { idx, monkey -> println("Monkey $idx inspected items ${monkey.inspectCount} times.")}
+            }
+
         }
-        monkeys.forEachIndexed { idx, monkey -> println("Monkey $idx inspected items ${monkey.inspectCount} times.")}
+        //monkeys.forEachIndexed { idx, monkey -> println("Monkey $idx inspected items ${monkey.inspectCount} times.")}
         val topMonkeys = monkeys.sortedBy { it.inspectCount }.reversed().take(2)
         val monkeyBusiness = topMonkeys.map { it.inspectCount } .reduce { acc, i -> acc * i }
         println("Amount of monkey business: $monkeyBusiness")
     }
     fun solve() {
         val monkeys = content.map{ it.trim() }.filter { it != "" }.chunked(6).map { it.toMonkey() }
-        part1(monkeys)
+//        doSolve(monkeys.toList(), 20)
+        doSolve(monkeys.toList(), 20, false)
     }
 }
