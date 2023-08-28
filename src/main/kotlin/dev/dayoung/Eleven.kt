ad187e3d65292b5c7fcf8654d3e7036c2e5e053d
David Young <ohnoitsyou7043@gmail.com>
2023-08-28 01:44:53 -0400

    Finished Day 11
    Part 1 works
    Part 2 works
    But implementing part 2 broke part 1? Not sure what broke

--- src/main/kotlin/dev/dayoung/Eleven.kt
+++ src/main/kotlin/dev/dayoung/Eleven.kt
@@ -1,61 +1,50 @@
 package dev.dayoung
 
-import kotlin.math.floor
+import java.math.BigInteger
 
 fun List<String>.toMonkey(): Eleven.Monkey {
     val number = this[0].substring("Monkey ".length, this[0].length - 1).toInt()
-    val items = this[1].substring(this[1].lastIndexOf(':') + 1).split(",").map { it.trim().toDouble() }
+    val items = this[1].substring(this[1].lastIndexOf(':') + 1).split(",").map { it.trim().toBigInteger() }
     val operation = this[2].substring(this[2].lastIndexOf('=') + 2).split(" ").map { it.trim()}
-    val test = this[3].substring(this[3].lastIndexOf(" ")).trim().toInt()
+    val test = this[3].substring(this[3].lastIndexOf(" ")).trim().toBigInteger()
     val t = this[4].substring(this[4].lastIndexOf(" ")).trim().toInt()
     val f = this[5].substring(this[5].lastIndexOf(" ")).trim().toInt()
     return Eleven.Monkey(number, items.toMutableList(), Eleven.Operation(operation[0], operation[1], operation[2]), Eleven.Test(test, t, f))
 }
 
 fun String.isNumeric() = this.toIntOrNull() != null
-fun String.operatorToIntOp(): Double.(Double) -> Double = when (this) {
-    "+" -> Double::plus
-    "-" -> Double::minus
-    "*" -> Double::times
-    "/","divisible" -> Double::div
+fun String.operatorToIntOp(): BigInteger.(BigInteger) -> BigInteger = when (this) {
+    "+" -> BigInteger::plus
+    "-" -> BigInteger::minus
+    "*" -> BigInteger::times
+    "/","divisible" -> BigInteger::div
     else -> error("Unknown operator $this")
 }
 class Eleven(sampleMode: Boolean) {
-    class Operation(val factor1: String, val operator: String, val factor2: String) {
-    }
-    class Test(val divisor: Int, val t: Int, val f: Int) { }
-    class Monkey(val number: Int, val items: MutableList<Double>, val operation: Operation, val test: Test) {
-        var inspectCount = 0
-        fun receiveItem(item: Double) {
+    data class Operation(val factor1: String, val operator: String, val factor2: String)
+    data class Test(val divisor: BigInteger, val t: Int, val f: Int)
+    data class Monkey(val number: Int, val items: MutableList<BigInteger>, val operation: Operation, val test: Test) {
+        var inspectCount = BigInteger("0")
+        fun receiveItem(item: BigInteger) {
             items.add(item)
         }
 
-        fun inspectItems(relief: Boolean = true): List<Pair<Int, Double>> {
-            val returnItems = mutableListOf<Pair<Int, Double>>()
-            for(item in items) {
-//                println("Inspecting item with a worry level of $item")
+        fun inspectItems(relief: Boolean = true, gcd: Int): List<Pair<Int, BigInteger>> {
+            val returnItems = mutableListOf<Pair<Int, BigInteger>>()
+            items.forEach {
                 inspectCount++
-                var newWorry = if (operation.factor2.isNumeric()) {
-                    operation.operator.operatorToIntOp()(item, operation.factor2.toDouble())
-                } else {
-                    operation.operator.operatorToIntOp()(item, item)
-                }
-                if(relief) {
-                    floor(newWorry)
-                    newWorry = floor(newWorry / 3.0)
-                }
-//                println("New worry level: $newWorry")
+                val item = if (operation.factor2.isNumeric()) operation.factor2.toBigInteger() else it
+                var newWorry = operation.operator.operatorToIntOp()(it,item) % gcd.toBigInteger()
 
-                if((newWorry % test.divisor) == 0.0) {
-//                    println("divisible by ${test.divisor}")
-                    returnItems.add(test.t to newWorry)
-//                    println("item with worry level $newWorry is thrown to ${test.t}")
-                } else {
-//                    println("not divisible by ${test.divisor}")
-                    returnItems.add(test.f to newWorry)
-//                    println("item with worry level $newWorry is thrown to ${test.f}")
+                if(relief) {
+                    newWorry /= BigInteger("3")
                 }
-//                println("========")
+
+                if((newWorry % test.divisor).toInt() == 0) {
+                    returnItems.add(test.t to newWorry)
+                } else {
+                    returnItems.add(test.f to newWorry)
+                }
             }
             items.clear()
             return returnItems
@@ -63,29 +52,28 @@ class Eleven(sampleMode: Boolean) {
     }
 
     private val content = Utils.readInputResource(sampleMode, "eleven.txt")
-    private fun doSolve(monkeys: List<Monkey>, rounds: Int, relief: Boolean = true) {
-        for(i in 0 until rounds) {
+    private fun doSolve(monkeys: List<Monkey>, rounds: Int, relief: Boolean = true, gcd: Int) {
+        for(i in 1 .. rounds) {
             for(monkey in monkeys) {
-                val thrownItems = monkey.inspectItems(relief)
+                val thrownItems = monkey.inspectItems(relief, gcd)
                 thrownItems.forEach { monkeys[it.first].receiveItem(it.second) }
             }
-            monkeys.forEach {
-                println("Monkey ${it.number}: ${it.items}")
+            if(relief) {
+                println("After round $i:")
+                monkeys.forEachIndexed { idx, it -> println("Monkey $idx ${it.items}")}
             }
-            if(i + 1 in listOf(1,20,1000,2000,3000,4000,5000,6000,7000,8000,9000,10000)) {
-                println("== After round ${i +1} ==")
-                monkeys.forEachIndexed { idx, monkey -> println("Monkey $idx inspected items ${monkey.inspectCount} times.")}
-            }
-
         }
-        //monkeys.forEachIndexed { idx, monkey -> println("Monkey $idx inspected items ${monkey.inspectCount} times.")}
-        val topMonkeys = monkeys.sortedBy { it.inspectCount }.reversed().take(2)
-        val monkeyBusiness = topMonkeys.map { it.inspectCount } .reduce { acc, i -> acc * i }
+        val topMonkeys = monkeys.sortedByDescending { it.inspectCount }.take(2)
+        val monkeyBusiness = topMonkeys.map { it.inspectCount }.reduce { acc, i -> acc * i }
         println("Amount of monkey business: $monkeyBusiness")
     }
     fun solve() {
-        val monkeys = content.map{ it.trim() }.filter { it != "" }.chunked(6).map { it.toMonkey() }
-//        doSolve(monkeys.toList(), 20)
-        doSolve(monkeys.toList(), 20, false)
+        val monkeys1 = content.map{ it.trim() }.filter { it != "" }.chunked(6).map { it.toMonkey() }
+        val monkeys2 = content.map{ it.trim() }.filter { it != "" }.chunked(6).map { it.toMonkey() }
+
+        // Chinese Remainder Theorem
+        val gcd = monkeys1.fold(1) { acc, monkey -> acc * monkey.test.divisor.toInt()  }
+//        doSolve(monkeys1.toList(), 20, true, gcd)
+        doSolve(monkeys2.toList(), 10000, false, gcd)
     }
 }
