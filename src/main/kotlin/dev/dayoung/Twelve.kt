40cd44a46357b93b1b95687bec2b6ee627ed30ca
David Young <ohnoitsyou7043@gmail.com>
2023-09-05 09:40:52 -0400

    Day 12 progress

--- src/main/kotlin/dev/dayoung/Twelve.kt
+++ src/main/kotlin/dev/dayoung/Twelve.kt
@@ -1,7 +1,7 @@
 package dev.dayoung
 
 class Twelve(sampleMode: Boolean) {
-    class Grid(input: List<String>) {
+    class Grid(input: List<String>) : Iterable<Point> {
         private val height = input.size
         private val width = input[0].length
         private val grid = input.fold("") { acc, line -> acc + line }.map { it.code }
@@ -10,25 +10,71 @@ class Twelve(sampleMode: Boolean) {
             println("Grid: ${grid.map { it.toChar() }}")
         }
 
-        private fun cellAt(p: Point): Int? =
-            if(p.first in 0 .. width || p.second in 0 .. height) grid[p.first + p.second * height] else null
+        override fun iterator(): Iterator<Point> {
+            return object : Iterator<Point> {
+                var x = -1
+                var y = -1
+
+                override fun hasNext(): Boolean {
+                    return x + y * width < grid.lastIndex
+                }
+
+                override fun next(): Point {
+                    val hitY = if((x + 1) % width == 0) y + 1 else y
+                    y = if((x + 1) % width == 0) y + 1 else y
+                    x = (x + 1) % width
+                    return x to y
+                }
+            }
+        }
+
+        fun cellAt(p: Point): Int? =
+            if(p.first in 0 until width &&
+               p.second in 0 until height &&
+               (p.first + p.second * width) < grid.size) grid[p.first + p.second * width] else null
         fun neighbors(cell: Point): List<Point> {
             return dirs.map { cell.first + it.first to cell.second + it.second }
                 .filter { cellAt(it) != null }
                 .filter { cellAt(cell) in (cellAt(it)!! - 1) .. (cellAt(it)!! + 1)}
         }
 
-        override fun toString(): String {
-            return (0 until height).foldIndexed("") { idy, yacc, y ->
-                yacc + (0 until width).foldIndexed("") { idx, xacc, x -> xacc + cellAt(idx to idy)?.toChar() } + "\n"
+        fun breadthSearch(grid: Grid, start: Point) {
+            val frontier = ArrayDeque<Point>()
+            frontier.add(start)
+            val reached = HashMap<Point, Boolean>()
+            reached[start] = true
+
+            while(frontier.isNotEmpty()) {
+                val current = frontier.first()
+                println("Visiting: $current")
+                for(next in neighbors(current)) {
+                    if(!reached.contains(next)) {
+                        frontier.add(next)
+                        reached[next] = true
+                    }
+                }
             }
         }
 
+        override fun toString(): String {
+            return (0 until height).foldIndexed("") { idy, yacc, y ->
+                yacc + (0 until width).foldIndexed("") {
+                        idx, xacc, _ -> xacc + cellAt(idx to idy)?.toChar()
+                } + "\n"
+            }
+        }
     }
     private val content = Utils.readInputResource(sampleMode, "twelve.txt")
     fun solve() {
-        println(content)
         val grid = Grid(content)
         println(grid)
+        var start = 0 to 0
+        for(cell in grid) {
+            println("$cell ${grid.cellAt(cell)?.toChar()} ${grid.neighbors(cell)}")
+            if(grid.cellAt(cell)?.toChar() == 'S') {
+               start = cell
+            }
+        }
+        grid.breadthSearch(grid, start)
     }
 }
